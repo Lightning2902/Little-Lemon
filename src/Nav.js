a2b1cc89462887de0a687b4681d7203a2e1ab54f
Lightning2902 <121048901+Lightning2902@users.noreply.github.com>
2024-09-12 08:37:00 +0530

    semantic tags

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Reservation</li>
+      </ul>
+    </>
+  )
+}
+
+export default Nav
